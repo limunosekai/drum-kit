5d52ff9bd717acf56220ab1e7a37321aeae837eb
limunosekai <powerlsh0103@gmail.com>
2021-06-09 17:09:24 +0900

    Add README

---
+++ README.md
@@ -0,0 +1,25 @@
+## Drum-kit
+
+[Go to WEB](https://limunosekai.github.io/drum-kit/)
+
+<br>
+
+### ✨ Main Logic
+
+![1](./image/K-001.png)
+
+<br>
+
+<br>
+
+### ✨ Screen Shot
+
+![2](./image/K-002.png)
+
+<br>
+
+<br>
+
+---
+
+Source : [JAVASCRIPT 30](https://javascript30.com/)
--- asset/js/main.js
+++ asset/js/main.js
@@ -1,17 +1,18 @@
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if(!audio) return; // 해당 key 이외의 값을 실행할 경우 stop
+  if (!audio) return; // 해당 key 이외의 값을 실행할 경우 stop
   audio.currentTime = 0; // term 없이 실행
   audio.play();
   key.classList.add('playing');
 }
 
 function removeTransition(e) {
-  if (e.propertyName !== 'transform') return; // event의 propertyName이 transform이 아닌 경우 skip
+  // event의 propertyName이 transform이 아닌 경우 skip
+  if (e.propertyName !== 'transform') return;
   this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
